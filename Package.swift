3ac94ba89f2b904ac7ab4691e6d28f5ec49f4ff0
lololo <guolei912@gmail.com>
2021-05-19 15:12:26 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,46 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "GCDWebServer",
-    platforms: [
-        .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
-    ],
     products: [
-        .library(name: "GCDWebServer", targets: ["GCDWebServer"])
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "GCDWebServer",
+            targets: ["GCDWebServer"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "GCDWebServer",
-                dependencies: [],
-                publicHeadersPath:"*.h"
-                )
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "GCDWebServer",
+            dependencies: [],
+            path: ".",
+            exclude: [
+                "Frameworks",
+                "iOS",
+                "Mac",
+                "Tests",
+                "tvOS",
+                "Package.swift",
+                "GCDWebServer.podspec",
+                "Run-Tests.sh",
+                "format-source.sh",
+                "README.md",
+                "LICENSE"
+            ],
+            resources: [
+                .copy("GCDWebUploader/GCDWebUploader.bundle"),
+            ],
+            cSettings:[
+                .headerSearchPath("GCDWebServer/Core"),
+                .headerSearchPath("GCDWebServer/Requests"),
+                .headerSearchPath("GCDWebServer/Responses"),
+            ]),
     ]
 )
